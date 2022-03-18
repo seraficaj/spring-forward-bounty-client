c5abef0f043d892081784920b6eb64fd0c0230a6
weston-bailey <wdbailey87@gmail.com>
2022-03-18 14:06:58 -0700

    debug

--- src/components/pages/Bounties.jsx
+++ src/components/pages/Bounties.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 export default function Bounties() {
+  // state to hold the bounties
+  const [bounties, setBounties] = useState([])
+  // state to control the form
+
   // use effect to get all the bounties 
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_SERVER_URL + '/bounties')
+    axios.get(process.env.REACT_APP_SERVER_URL + '/bounties')
+      .then(response => {
+       setBounties(response.data)
+      })
+  }, []) // empty dep array so it only runs once
 
   // controlled form to create a new bounty
 
   // render all the bounties with links to /bounties/:id
+  const bountyLinks = bounties.map((bounty, idx) => {
+    return (
+      <div key={`bounty-link${idx}`}>
+        {/* front end link in react */}
+        <Link to={`/bounties/${bounty._id}`}>{bounty.name}</Link>
+      </div>
+    )
+  })
   return (
     <>
-      Bounties component
+      this will be a form
+      {bountyLinks}
     </>
   )
 }
